d8d77f7c32c90f887ddf92970c86cb156e505b87
javaharreddy <89154426+javaharreddy@users.noreply.github.com>
2023-02-25 11:16:01 +0530

    Create prog25_mrng.py

---
+++ prog25_mrng.py
@@ -0,0 +1,6 @@
+while(int(input('enter 1 to calculate else enter 0: '))):
+    x=int(input('enter the numerator part of x/y th of sams weekly income: '))
+    y=int(input('enter the denominator part of x/y th of sams weekly income: '))
+    m=int(input('enter the x/y th of sams weekly income: '))
+    z=int(input('enter the denominator of 1/z th of sams weekly income to find :'))
+    print('sams 1/z th part of weekly income is : '+ str((m*y)/(z*x)))
